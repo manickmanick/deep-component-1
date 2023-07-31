039175d0c7f5799cb9c1fb2d35d49167b91a7903
manickmanick <manickaperumal22@gmail.com>
2023-07-31 20:31:17 +0530

    initial commit

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-prop="xyz"></slot>
+        </li>
+    </ul>
+</template>
+
+
+<script>
+export default {
+    data ()
+    {
+        return {
+            goals: ["finish course", "learn vue"]
+        }
+    }
+}
+</script>
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+        <h2>Manage Goals</h2>
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    methods: {
+        setGoal ()
+        {
+            const enteredValue = this.$refs.goal
+            if (enteredValue === "")
+            {
+                alert("type something")
+            }
+        }
+    }
+}
+</script>
